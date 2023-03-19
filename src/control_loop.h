585f951b141c2711727d6f990d0e6aab785d5466
giobuiano99 <gi.buiano@studenti.unina.it>
2023-03-19 13:27:28 +0100

    commit control loop

--- src/control_loop.h
+++ src/control_loop.h
@@ -1,15 +1,26 @@
 #include <iostream>
 #include "boost/thread.hpp"
+#include <fstream>
+#include <unistd.h>
 
 using namespace std;
 
 class CONTROLLER {
     public:
-        CONTROLLER();
+        CONTROLLER(const float &,const float &,const float &,const float &,float);
         
         void loop();                //Main loop function        
         void system_start();       //start the system
-        void set_xdes(double x);   //member to set the desired value
+        void set_xdes(float);   //member to set the desired value
+        bool get_q(); 
 
     private:
+
+        float _Kp;
+        float _Ki;
+        float _Kd;
+        float _initial_value;
+        float _cmd;
+        float _xdes;
+        bool _q; //boolean value to determinate when the loop is terminated
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,29 @@ using namespace std;
 
 //the goal is to implement a simple PID controller, 
 //running on input value to reach the desired one
+//we have implemented a transfer function to simulate a system which can be controlled
+//by our PID
 
 // Sense: read a value from keyboard
 // Plan:  generate the correct input
 // Act:   set the input
 
 int main(int argc, char** argv) {
+    float initial_controller_value;
+    float xdes;
+    if( argc >1 ){
+        initial_controller_value=atof(argv[1]);
+    }else initial_controller_value=0.0;
 
+    cout<<"inserisci riferimento"<<endl;
+    cin>> xdes;
+
+    CONTROLLER c(0.4,initial_controller_value,0.4,0.002,xdes);
+  
+    while (c.get_q()){
+        cin>>xdes;
+        c.set_xdes(xdes);
+    }
 
     return 0;
 }
